11ab83ea249707d9f5fe301f5196caf30edcd457
Palvai Kaushik Reddy <kaushikpalvai2004@gmail.com>
2025-02-21 16:48:45 +0530

    frontend added

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-# from flask import Flask, render_template, request, redirect, url_for
-# import os
+from flask import Flask,request,redirect,render_template
 
-# app = Flask(__name__)
-# UPLOAD_FOLDER = 'uploads'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app=Flask(__name__)
 
-# if not os.path.exists(UPLOAD_FOLDER):
-#     os.makedirs(UPLOAD_FOLDER)
-
-# @app.route('/', methods=['GET', 'POST'])
-# def home():
-#     if request.method == 'POST':
-#         files = request.files.getlist('resume')
-#         for file in files:
-#             if file.filename != '':
-#                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-#         return redirect(url_for('home'))
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/')
+def home():
+    return render_template('/home.html')
 
 
+
+@app.route('/upload')
+def upload():
+    return render_template('/upload.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('/about.html')
+
+
+
+if __name__=="__main__":
+    app.run(debug=True,port=4000)
